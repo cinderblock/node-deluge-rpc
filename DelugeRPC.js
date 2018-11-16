10f98dcb9da1e1230b8261a746c4a67f37230bc4
Cameron Tacklind <cameron@tacklind.com>
2018-11-16 12:15:19 -0800

    Implement API functions

--- DelugeRPC.js
+++ DelugeRPC.js
@@ -174,32 +174,104 @@ function DelugeRPC(socket, options) {
     return { result, sent };
   }
 
-  function getVersion() {
-    // TODO handle rejected to automatically detect version 1-ish
-    return request('daemon.info');
-  }
-
-  let authLevel = 0;
-
-  function login(username, password) {
-    const res = request('daemon.login', [username, password]);
-
-    res.result.then(level => {
-      authLevel = level;
-    });
-
-    return res;
-  }
-
-  function getAuthLevel() {
-    return authLevel;
-  }
-
-  function addTorrentUrl(url, options, headers) {
-    return request('core.add_torrent_url', [url, options, headers]);
-  }
-
-  return { request, events, login, getVersion, getAuthLevel, addTorrentUrl };
+  return {
+    request,
+    events,
+    core: {
+      addTorrentFile: (filename, filedump, torrentOptions) =>
+        request('core.add_torrent_file', [filename, filedump, torrentOptions]),
+      addTorrentUrl: (url, torrentOptions, options) => request('core.add_torrent_url', [url, torrentOptions], options),
+      addTorrentMagnet: (uri, torrentOptions) => request('core.add_torrent_magnet', [uri, torrentOptions]),
+      removeTorrent: (torrentId, removeData) => request('core.remove_torrent', [torrentId, removeData]),
+      getSessionStatus: keys => request('core.get_session_status', [keys]),
+      getCacheStatus: () => request('core.get_cache_status', []),
+      forceReannounce: torrentIds => request('core.force_reannounce', [torrentIds]),
+      pauseTorrent: torrentIds => request('core.pause_torrent', [torrentIds]),
+      connectPeer: (torrentId, ip, port) => request('core.connect_peer', [torrentId, ip, port]),
+      moveStorage: (torrentIds, dest) => request('core.move_storage', [torrentIds, dest]),
+      pauseAllTorrents: () => request('core.pause_all_torrents', []),
+      resumeAllTorrents: () => request('core.resume_all_torrents', []),
+      resumeTorrent: torrentIds => request('core.resume_torrent', [torrentIds]),
+      getTorrentStatus: (torrentId, keys, options) => request('core.get_torrent_status', [torrentId, keys], options),
+      getTorrentsStatus: (filter_dict, keys, options) =>
+        request('core.get_torrents_status', [filter_dict, keys], options),
+      getFilterTree: options => request('core.get_filter_tree', options),
+      getSessionState: () => request('core.get_session_state', []),
+      getConfig: () => request('core.get_config', []),
+      getConfigValue: key => request('core.get_config_value', [key]),
+      getConfigValues: keys => request('core.get_config_values', [keys]),
+      setConfig: config => request('core.set_config', [config]),
+      getListenPort: () => request('core.get_listen_port', []),
+      getNumConnections: () => request('core.get_num_connections', []),
+      getAvailablePlugins: () => request('core.get_available_plugins', []),
+      getEnabledPlugins: () => request('core.get_enabled_plugins', []),
+      enablePlugin: plugin => request('core.enable_plugin', [plugin]),
+      disablePlugin: plugin => request('core.disable_plugin', [plugin]),
+      forceRecheck: torrentIds => request('core.force_recheck', [torrentIds]),
+      setTorrentOptions: (torrentIds, torrentOptions) =>
+        request('core.set_torrent_options', [torrentIds, torrentOptions]),
+      setTorrentTrackers: (torrentId, trackers) => request('core.set_torrent_trackers', [torrentId, trackers]),
+      setTorrentMaxConnections: (torrentId, value) => request('core.set_torrent_max_connections', [torrentId, value]),
+      setTorrentMaxUploadSlots: (torrentId, value) => request('core.set_torrent_max_upload_slots', [torrentId, value]),
+      setTorrentMaxUploadSpeed: (torrentId, value) => request('core.set_torrent_max_upload_speed', [torrentId, value]),
+      setTorrentMaxDownloadSpeed: (torrentId, value) =>
+        request('core.set_torrent_max_download_speed', [torrentId, value]),
+      setTorrentFilePriorities: (torrentId, priorities) =>
+        request('core.set_torrent_file_priorities', [torrentId, priorities]),
+      setTorrentPrioritizeFirstLast: (torrentId, value) =>
+        request('core.set_torrent_prioritize_first_last', [torrentId, value]),
+      setTorrentAutoManaged: (torrentId, value) => request('core.set_torrent_auto_managed', [torrentId, value]),
+      setTorrentStopAtRatio: (torrentId, value) => request('core.set_torrent_stop_at_ratio', [torrentId, value]),
+      setTorrentStopRatio: (torrentId, value) => request('core.set_torrent_stop_ratio', [torrentId, value]),
+      setTorrentRemoveAtRatio: (torrentId, value) => request('core.set_torrent_remove_at_ratio', [torrentId, value]),
+      setTorrentMoveCompleted: (torrentId, value) => request('core.set_torrent_move_completed', [torrentId, value]),
+      setTorrentMoveCompletedPath: (torrentId, value) =>
+        request('core.set_torrent_move_completed_path', [torrentId, value]),
+      getPathSize: path => request('core.get_path_size', [path]),
+      createTorrent: (
+        path,
+        tracker,
+        piece_length,
+        comment,
+        target,
+        webseeds,
+        priv,
+        created_by,
+        trackers,
+        add_to_session
+      ) =>
+        request('core.create_torrent', [
+          path,
+          tracker,
+          piece_length,
+          comment,
+          target,
+          webseeds,
+          priv,
+          created_by,
+          trackers,
+          add_to_session,
+        ]),
+      uploadPlugin: (filename, filedump) => request('core.upload_plugin', [filename, filedump]),
+      rescanPlugins: () => request('core.rescan_plugins', []),
+      renameFiles: () => request('core.rename_files', []),
+      renameFolder: () => request('core.rename_folder', []),
+      queueTop: () => request('core.queue_top', []),
+      queueUp: () => request('core.queue_up', []),
+      queueDown: () => request('core.queue_down', []),
+      queueBottom: () => request('core.queue_bottom', []),
+      glob: () => request('core.glob', []),
+      testListenPort: () => request('core.test_listen_port'),
+      getFreeSpace: () => request('core.get_free_space', []),
+      getLibtorrentVersion: () => request('core.get_libtorrent_version', []),
+    },
+    daemon: {
+      getMethodList: () => request('daemon.get_method_list'),
+      info: () => request('daemon.info'),
+      shutdown: () => request('daemon.shutdown'),
+      login: (username, password) => request('daemon.login', [username, password]),
+    },
+  };
 }
 
 module.exports = DelugeRPC;
