aac8c5a4e1b8f885ec191bfc924ce45262d7c653
Cameron Tacklind <cameron@tacklind.com>
2018-11-15 18:23:25 -0800

    Update examples

--- repl.js
+++ repl.js
@@ -1,10 +1,10 @@
 console.log();
 console.log('Cheat sheet:');
-console.log("  (typeof c != 'undefined' && c.end()); (rpc = DelugeRPC(c = tls.connect(config)) ) && true");
-console.log('  res = rpc.login(config.user, config.password)');
 console.log(
-  "  rpc.request('daemon.info').result.catch(() => {console.log('Looks like v1.x')}).then(console.log) && true"
+  "  (typeof c != 'undefined' && c.end()); (rpc = DelugeRPC(c = tls.connect(config)) ) && rpc.events.on('delugeEvent', console.log) && true"
 );
+console.log('  res = rpc.login(config.user, config.password)');
+console.log("  rpc.getVersion().result.catch(() => {console.log('Looks like v1.x')}).then(console.log) && true");
 console.log('  c.end()');
 console.log();
 
