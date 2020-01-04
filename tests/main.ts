74881f3f895fc4ce57681c4f2e17f4d55a5581a4
Cameron Tacklind <cameron@tacklind.com>
2020-01-03 20:59:51 -0800

    Maybe tests work?

--- tests/main.ts
+++ tests/main.ts
@@ -6,43 +6,79 @@ import DelugeRPC from '../src/DelugeRPC.js';
 
 import { SharedPromise } from './utils/SharedPromise';
 
-const port = Number(process.env.TEST_DELUGE_PORT) || 58846;
-const host = process.env.TEST_DELUGE_HOST || 'localhost';
+const port1 =
+  Number(process.env.DELUGE1_PORT || process.env.DELUGE_PORT) || 58846;
+
+const host1 =
+  process.env.DELUGE1_HOST || process.env.DELUGE_HOST || 'localhost';
+
+const version1 =
+  (process.env.DELUGE1_VERSION || process.env.DELUGE_VERSION) ?? 0 ? 1 : 0;
+
+const port2 = Number(process.env.DELUGE2_PORT);
+const host2 = process.env.DELUGE2_HOST || 'localhost';
 
 const options = {
   rejectUnauthorized: false,
 };
 
-const socket = SharedPromise<tls.TLSSocket>();
-
-const connected = SharedPromise();
-
-const RPC = SharedPromise<ReturnType<typeof DelugeRPC>>();
-
-test('Connect', async () => {
-  socket.resolve(
-    tls.connect(port, host, options).on('secureConnect', connected.resolve)
-  );
-
-  await connected.promise;
+testDeluge({
+  connect: { host: host1, port: port1 },
+  options,
+  expectedVersion: version1,
+  name: port2 ? 'Deluge 1.x' : undefined,
 });
 
-test('Wrap with Daemon', async () => {
-  RPC.resolve(DelugeRPC(await socket.promise));
+if (port2) {
+  testDeluge({
+    connect: { host: host2, port: port2 },
+    options,
+    expectedVersion: 1,
+    name: 'Deluge 2.x',
+  });
+}
 
-  await RPC.promise;
-});
+type Options = {
+  connect: { host: string; port: number };
+  options?: tls.ConnectionOptions;
+  expectedVersion: 0 | 1;
+  name?: string;
+};
 
-test('Get version', async () => {
-  await connected;
-  const rpc = await RPC.promise;
+function testDeluge({ connect, options, expectedVersion, name }: Options) {
+  name += ' - ';
 
-  const { sent, result } = rpc.daemon.info();
+  const socket = SharedPromise<tls.TLSSocket>();
 
-  await sent;
+  const connected = SharedPromise();
 
-  const res = await result;
+  const RPC = SharedPromise<ReturnType<typeof DelugeRPC>>();
 
-  console.log('res');
-  console.log(res);
-});
+  test(name + 'Connect', async () => {
+    socket.resolve(
+      tls.connect(port1, host1, options).on('secureConnect', connected.resolve)
+    );
+
+    await connected.promise;
+  });
+
+  test(name + 'Wrap with Daemon', async () => {
+    RPC.resolve(DelugeRPC(await socket.promise));
+
+    await RPC.promise;
+  });
+
+  test(name + 'Get version', async () => {
+    await connected;
+    const rpc = await RPC.promise;
+
+    const { sent, result } = rpc.daemon.info();
+
+    await sent;
+
+    const res = await result;
+
+    console.log('res');
+    console.log(res);
+  });
+}
